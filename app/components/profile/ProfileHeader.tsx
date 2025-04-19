dbffb0357d37159ea8b3a31e460a887ef050659c
germanl_git2310 <germanlamelar@hotmail.com>
2025-04-19 01:46:03 -0300

    [FIX] [ProfileHeader] Remove unused properties for svg

--- app/components/profile/ProfileHeader.tsx
+++ app/components/profile/ProfileHeader.tsx
@@ -39,11 +39,9 @@ export function ProfileHeader({ walletAddress, username }: ProfileHeaderProps) {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_bgCarrier"></g>
               <g
                 id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
               ></g>
               <g id="SVGRepo_iconCarrier">
                 {" "}
