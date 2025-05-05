f3bfba1f06e0d27f7e2791637a7a96570232bd8b
germanl_git2310 <germanlamelar@hotmail.com>
2025-05-05 19:43:39 -0300

    [FEATURE] [LoreEntryDetails Object] Add current votes for every loreEntry

--- types/FormattedData/lore-entry-details.ts
+++ types/FormattedData/lore-entry-details.ts
@@ -6,7 +6,8 @@ export interface LoreEntryDetails {
   loreEntry: LoreEntry
   userDetails: UserDetails
   nft: NFT
-  userVote: 1 | -1 | null
+  userVote: 1 | -1 | null,
+  votes: number
 } 
 
 export const sampleLoreEntryDetails: LoreEntryDetails[] = [
@@ -14,18 +15,21 @@ export const sampleLoreEntryDetails: LoreEntryDetails[] = [
       loreEntry: sampleLoreEntries[0],
       nft: sampleNFTs[0],
       userDetails: sampleUserDetails[0],
-      userVote: null
+      userVote: null,
+      votes: 0
     },
     {
       loreEntry: sampleLoreEntries[1],
       nft: sampleNFTs[1],
       userDetails: sampleUserDetails[1],
-      userVote: null
+      userVote: null,
+      votes: 0
     },
     {
       loreEntry: sampleLoreEntries[2],
       nft: sampleNFTs[2],
       userDetails: sampleUserDetails[2],
-      userVote: null
+      userVote: null,
+      votes: 0
     }
   ]
