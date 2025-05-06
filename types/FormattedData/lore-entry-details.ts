c30c1c612e1f00ff366faa9dd97b094cce397f24
germanl_git2310 <germanlamelar@hotmail.com>
2025-05-06 12:16:56 -0300

    [FEATURE] [LoreEntryDetails type] Remove votes property from LoreEntryDetails

--- types/FormattedData/lore-entry-details.ts
+++ types/FormattedData/lore-entry-details.ts
@@ -6,8 +6,7 @@ export interface LoreEntryDetails {
   loreEntry: LoreEntry
   userDetails: UserDetails
   nft: NFT
-  userVote: 1 | -1 | null,
-  votes: number
+  userVote: 1 | -1 | null
 } 
 
 export const sampleLoreEntryDetails: LoreEntryDetails[] = [
@@ -15,21 +14,18 @@ export const sampleLoreEntryDetails: LoreEntryDetails[] = [
       loreEntry: sampleLoreEntries[0],
       nft: sampleNFTs[0],
       userDetails: sampleUserDetails[0],
-      userVote: null,
-      votes: 0
+      userVote: null
     },
     {
       loreEntry: sampleLoreEntries[1],
       nft: sampleNFTs[1],
       userDetails: sampleUserDetails[1],
-      userVote: null,
-      votes: 0
+      userVote: null
     },
     {
       loreEntry: sampleLoreEntries[2],
       nft: sampleNFTs[2],
       userDetails: sampleUserDetails[2],
-      userVote: null,
-      votes: 0
+      userVote: null
     }
   ]
