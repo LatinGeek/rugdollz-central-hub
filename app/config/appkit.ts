9246b82625689c648cf1b30d3ccb7e3fb2ed812e
germanl_git2310 <germanlamelar@hotmail.com>
2025-05-09 19:22:16 -0300

    [FEATURE] [AppKit] Add sepolia to supported networks

--- app/config/appkit.ts
+++ app/config/appkit.ts
@@ -1,6 +1,6 @@
 import { cookieStorage, createStorage } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
 export const projectId = "09b3143d3ad0337592f999922e5de546"
@@ -9,7 +9,7 @@ if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [mainnet, arbitrum]
+export const networks = [mainnet, arbitrum, sepolia]
 
 // Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
