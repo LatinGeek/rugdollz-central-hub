55aa4291f27a56f68a08f9169ad6cdad530f63fb
germanl_git2310 <germanlamelar@hotmail.com>
2025-05-08 21:10:34 -0300

    [FEATURE] [NFTLayerCategory type] Update Type properties

--- types/Entities/nft-layer-category.ts
+++ types/Entities/nft-layer-category.ts
@@ -1,33 +1,12 @@
 export interface NFTLayerCategory {
   id: string;
   name: string;
-  layerOptionIds: string[];
 }
 
 export const sampleNFTLayerCategories: NFTLayerCategory[] = [
   {
-    id: "background",
+    id: "1",
     name: "Background",
-    layerOptionIds: ["1", "2", "3", "4", "5"],
   },
-  {
-    id: "body",
-    name: "Body",
-    layerOptionIds: ["6", "7", "8"],
-  },
-  {
-    id: "face",
-    name: "Face",
-    layerOptionIds: ["9", "10", "11"],
-  },
-  {
-    id: "accessories",
-    name: "Accessories",
-    layerOptionIds: ["12", "13", "14"],
-  },
-  {
-    id: "other",
-    name: "Other",
-    layerOptionIds: ["15", "16", "17"],
-  },
+
 ];
