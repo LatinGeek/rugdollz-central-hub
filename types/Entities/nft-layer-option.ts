2939dcc035af414fc1ad9e82db0d74e606037baf
germanl_git2310 <germanlamelar@hotmail.com>
2025-05-08 21:10:45 -0300

    [FEATURE] [NFTLayerOption type] Update type properties

--- types/Entities/nft-layer-option.ts
+++ types/Entities/nft-layer-option.ts
@@ -1,7 +1,12 @@
+import { CollectionType } from "../enums/collection";
+
 export interface NFTLayerOption {
   id: string;
   name: string;
   imageUrl: string;
+  categoryId: string;
+  allowedCollections: CollectionType[];
+  allowedTokenIds: string[]
 }
 
 export const sampleNFTLayerOptions: NFTLayerOption[] = [
@@ -9,80 +14,24 @@ export const sampleNFTLayerOptions: NFTLayerOption[] = [
     id: "1",
     name: "Halloween",
     imageUrl: "/images/sample-nft-layers/Halloween_Overlay_sample_1.png",
+    categoryId: "1",
+    allowedCollections: [],
+    allowedTokenIds: []
   },
   {
     id: "2",
     name: "Valentine",
     imageUrl: "/images/sample-nft-layers/Valentines_Overlay_sample.png",
+    categoryId: "1",
+    allowedCollections: [],
+    allowedTokenIds: []
   },
   {
     id: "3",
     name: "City",
     imageUrl: "/images/sample-nft-layers/xmas_overlay_1.png",
-  },
-
- 
-  {
-    id: "6",
-    name: "Classic",
-    imageUrl: "/images/nft/layers/body/classic.png",
-  },
-  {
-    id: "7",
-    name: "Cyber",
-    imageUrl: "/images/nft/layers/body/cyber.png",
-  },
-  {
-    id: "8",
-    name: "Royal",
-    imageUrl: "/images/nft/layers/body/royal.png",
-  },
-
-  {
-    id: "9",
-    name: "Happy",
-    imageUrl: "/images/nft/layers/face/happy.png",
-  },
-  {
-    id: "10",
-    name: "Serious",
-    imageUrl: "/images/nft/layers/face/serious.png",
-  },
-  {
-    id: "11",
-    name: "Wink",
-    imageUrl: "/images/nft/layers/face/wink.png",
-  },
-
-  {
-    id: "12",
-    name: "Glasses",
-    imageUrl: "/images/nft/layers/accessories/glasses.png",
-  },
-  {
-    id: "13",
-    name: "Hat",
-    imageUrl: "/images/nft/layers/accessories/hat.png",
-  },
-  {
-    id: "14",
-    name: "Necklace",
-    imageUrl: "/images/nft/layers/accessories/necklace.png",
-  },
-
-  {
-    id: "15",
-    name: "Pipe",
-    imageUrl: "/images/nft/layers/accessories/glasses.png",
-  },
-  {
-    id: "16",
-    name: "Cigar",
-    imageUrl: "/images/nft/layers/accessories/hat.png",
-  },
-  {
-    id: "17",
-    name: "Necklace",
-    imageUrl: "/images/nft/layers/accessories/necklace.png",
+    categoryId: "1",
+    allowedCollections: [],
+    allowedTokenIds: []
   },
 ];
