be0eb934c44a77407dba6f077ee7d7b313faefd8
germanl_git2310 <germanlamelar@hotmail.com>
2025-05-08 21:11:29 -0300

    [CHORE] [NFTLayerCategoryDetails Type] Update sample data

--- types/FormattedData/nft-layer-category-details.ts
+++ types/FormattedData/nft-layer-category-details.ts
@@ -10,31 +10,7 @@ export const sampleNFTLayerCategoryDetails: NFTLayerCategoryDetails[] = [
   {
     nftLayerCategory: sampleNFTLayerCategories[0],
     nftLayerOptions: sampleNFTLayerOptions.filter((option) =>
-      sampleNFTLayerCategories[0].layerOptionIds.includes(option.id)
+      option.categoryId === sampleNFTLayerCategories[0].id
     ),
-  },
-  {
-    nftLayerCategory: sampleNFTLayerCategories[1],
-    nftLayerOptions: sampleNFTLayerOptions.filter((option) =>
-      sampleNFTLayerCategories[1].layerOptionIds.includes(option.id)
-    ),
-  },
-  {
-    nftLayerCategory: sampleNFTLayerCategories[2],
-    nftLayerOptions: sampleNFTLayerOptions.filter((option) =>
-      sampleNFTLayerCategories[2].layerOptionIds.includes(option.id)
-    ),
-  },
-  {
-    nftLayerCategory: sampleNFTLayerCategories[3],
-    nftLayerOptions: sampleNFTLayerOptions.filter((option) =>
-      sampleNFTLayerCategories[3].layerOptionIds.includes(option.id)
-    ),
-  },
-  {
-    nftLayerCategory: sampleNFTLayerCategories[4],
-    nftLayerOptions: sampleNFTLayerOptions.filter((option) =>
-      sampleNFTLayerCategories[4].layerOptionIds.includes(option.id)
-    ),
-  },
+  }
 ];
